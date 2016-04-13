f9e72ac63000dac4acba7a0b6fccbd453f9b4853
wanqgiang <wangqiang@mapbar.com>
2016-04-13 11:24:29 +0800

    Init

---
+++ ExAuto/Demo.swift
@@ -0,0 +1,18 @@
+//
+//  Demo.swift
+//  ExAuto
+//
+//  Created by Ethan on 16/4/13.
+//  Copyright © 2016年 AppStudio. All rights reserved.
+//
+
+import UIKit
+
+public class Demo: NSObject {
+
+    public func printLog() {
+        
+        print("Log")
+        
+    }
+}
